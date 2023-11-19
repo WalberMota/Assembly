f52c38396715e9d35321e0d0d4c8dc0624a06fce
Walber S Mota <mota.csharp@gmail.com>
2023-11-19 19:25:00 -0300

    update hello.asm

--- Aula01/hello.asm
+++ Aula01/hello.asm
@@ -5,8 +5,9 @@
 
 
 section .data
-    msg db "Ola assembly!"
-    
+    msg db "Ola assembly!"  ;referencia para criar a string 
+    tam equ $- msg          ;obtém o tamanho da msg
+
 section .bss
 
 section .text
@@ -14,6 +15,11 @@ section .text
 global _start
 
 _start:
+    mov     EAX,0x4     ; instrução de mandar algo para stdout (saída padrão)
+    mov     EBX, 0x1    ; informa saída padrão
+    mov     ECX,msg     ;
+    mov     EDX,tam     ;
+
     ;as três instruções abaixo são obrigatórias para encerrar qualquer programa nasm
     mov     eax,0x1 ;terminando o programa
     mov     ebx,0x0 ;return 0
