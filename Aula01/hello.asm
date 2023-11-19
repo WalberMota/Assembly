c965aacc3fd374cc2d7b5229898d369e45ed61b3
Walber S Mota <mota.csharp@gmail.com>
2023-11-19 20:48:04 -0300

    update hello.asm

--- Aula01/hello.asm
+++ Aula01/hello.asm
@@ -19,10 +19,10 @@ _start:
     mov     EBX, 0x1    ; informa saída padrão,System.out. 0x0 para entrada de valor, System.in
     mov     ECX,msg     ;
     mov     EDX,tam     ;
-    int 0x80
+    int 0x80            ;Envia a informacao ao Sistema Operacional (kernel)
 
 saida:
     ;as três instruções abaixo são obrigatórias para encerrar qualquer programa nasm
     mov     eax,0x1 ;Indica o final de operação, corresponde a System.exit
-    mov     ebx,0x0 ;return 0
+    mov     ebx,0x0 ;Informa o estado final do programa - 0 sem erro
     int     0x80    ;implemente chamadas de sistema(syscall), uma forma de os programas do espaço do usuário se comunicarem com o kernel.
