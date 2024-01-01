85d56e13564acc2274a23d754066ada86b848216
WalberMota <mota.csharp@gmail.com>
2024-01-01 20:06:41 -0300

    update saida.asm RaySeyfarth

--- RaySeyfarth/saida.asm
+++ RaySeyfarth/saida.asm
@@ -10,7 +10,7 @@ global _start
 
 _start :
         mov eax,1 ;1 é o número de chamada do sistema de saída
-        mov ebx,5 ;o valor do status a ser retornado
+        mov ebx,3 ;o valor do status a ser retornado - se mudar aqui muda em "echo $?"
         int 0x80  ;executar uma chamada de sistema
 
 
