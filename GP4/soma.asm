51552153b60e47d4501977c616ad7bd8be6c90aa
WalberMota <mota.csharp@gmail.com>
2024-01-01 19:35:05 -0300

    update soma.asm

--- GP4/soma.asm
+++ GP4/soma.asm
@@ -4,8 +4,8 @@
 section .data
     num1 dd 10
     num2 dd 20
-    fmt db 'A sma de %d e %d é %d', 10, 20
-    sum dd 00
+    fmt db 'A sma de %d e %d é %d', 10, 0
+    ;sum dd 00
 
 section .text
     global _start
@@ -17,16 +17,12 @@ _start:
     mov [sum], eax
 
     ;imprime o resultado
-    ;push dword[sum]
-    ;push dword[num2]
-    ;push dword[num1]
-    ;push dword fmt
-    ;call printf
-    mov     EAX,0x4     ; instrução de mandar algo para stdout (saída padrão)(ox3=read; 0x4=write)
-    mov     EBX, 0x1    ; informa saída padrão,System.out. 0x0 para entrada de valor, System.in
-    mov     ECX,[sum]     ;
-    ;mov     EDX,tam     ;
-    int 0x80
+    push dword[sum]
+    push dword[num2]
+    push dword[num1]
+    push dword fmt
+    call printf
+
 
 
 
