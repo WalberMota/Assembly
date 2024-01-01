8ae73331e6f1bcf10c1e71bef0ecdf3e4129bf48
WalberMota <mota.csharp@gmail.com>
2024-01-01 19:55:59 -0300

    criado RaySeyfarth folder

---
+++ Ray Seyfarth/saida.asm
@@ -0,0 +1,17 @@
+;Program : saida.asm
+;executa uma saida na chamada do sistema
+;Sem entradas (inputs)
+;saida : apenas a saida no status use ($? no shell)
+
+
+segment . text
+
+global _start
+
+_start :
+        mov eax,1 ;1 é o número de chamada do sistema de saída
+        mov ebx,5 ;o valor do status a ser retornado
+        int Ox80  ;executar uma chamada de sistema
+
+the status value to return
+
