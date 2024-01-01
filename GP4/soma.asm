ac551f3b0b9691657e4107a04367c56c04bb5df3
WalberMota <mota.csharp@gmail.com>
2024-01-01 18:49:59 -0300

    update soma.asm

--- GP4/soma.asm
+++ GP4/soma.asm
@@ -4,4 +4,22 @@ section .data
     fmt db 'A sma de %d e %d é %d', 10, 0
 
 section .text
-    global 
+    global _start
+
+_start:
+
+    mov eax, [num1]
+    add eax,[num2]
+    mov [sum], eax
+
+    ;imprime o resultado
+    push dword[sum]
+    push dword[num2]
+    push dword[num1]
+    push dword fmt
+    call printf
+
+    ;Sai do programaa
+    mov eax,1
+    xor ebx,ebx
+    int 0x80
