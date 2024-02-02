1600d84690dd8e6217222a9a55e7a9abfe45ffd7
WalberMota <mota.csharp@gmail.com>
2024-02-01 22:20:20 -0300

    update saida.asm

--- RaySeyfarth/saida.asm
+++ RaySeyfarth/saida.asm
@@ -2,7 +2,7 @@
 ;executa uma saida na chamada do sistema
 ;Sem entradas (inputs)
 ;saida : apenas a saida no status use ($? no shell)
-;compilar: 'nasm -f elf64 saida.asm' e 'ld saida.o -o saida'
+;compilar: 'nasm -f elf64 saida.asm' e transformar em executavel com 'ld saida.o -o saida'
 
 
 segment .text
