6933ec58d629e54dd350e5f726252df3af0b78c9
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-02-18 20:35:28 -0300

    update somaDeDoisInts.asm

--- udemy/somaDeDoisInts.asm
+++ udemy/somaDeDoisInts.asm
@@ -39,7 +39,7 @@ _start:
     int 0x80  
 
 saida:
-    ;as três instruções abaixo são obrigatórias para encerrar qualquer programa nasm
+    ;instruções obrigatórias para encerrar qualquer programa nasm
     mov     eax,0x1 ;Indica o final de operação, corresponde a System.exit
     mov     ebx,0x0 ;Informa o estado final do programa - 0 sem erro
     int     0x80    ;implemente chamadas de sistema(syscall), uma forma de os programas do espaço do usuário se comunicarem com o kernel.
