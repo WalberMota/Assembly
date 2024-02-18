2f6e333a172bfe11999b3a675a74682b090cf749
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-02-18 20:24:00 -0300

    update somaDeDoisInts.asm

--- udemy/somaDeDoisInts.asm
+++ udemy/somaDeDoisInts.asm
@@ -1,14 +1,14 @@
-.386
-.model flat,stdcall
-.stack 4096
-.code
-main Proc
+;.386
+;.model flat,stdcall
+;.stack 4096
+;.code
+;main Proc
     mov eax, 10000h
     add eax, 40000h
     sub eax, 20000h
     int 0x80  
     ret
-main ENDP
-END main
+;main ENDP
+;END main
 
 
