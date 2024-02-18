2408d6ca2c5949e26c451d4863c4466cc4b7a2e6
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-02-18 20:26:38 -0300

    update somaDeDoisInts.asm

--- udemy/somaDeDoisInts.asm
+++ udemy/somaDeDoisInts.asm
@@ -16,4 +16,10 @@
     add eax, 40000h
     sub eax, 20000h
     int 0x80  
-    ret
+
+
+saida:
+    ;as três instruções abaixo são obrigatórias para encerrar qualquer programa nasm
+    mov     eax,0x1 ;Indica o final de operação, corresponde a System.exit
+    mov     ebx,0x0 ;Informa o estado final do programa - 0 sem erro
+    int     0x80    ;implemente chamadas de sistema(syscall), uma forma de os programas do espaço do usuário se comunicarem com o kernel.
