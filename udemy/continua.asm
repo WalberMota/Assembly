dcf155e55942539ca0971aeebc303ac981fb3026
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-02-23 20:11:51 -0300

    update continua.asm

--- udemy/continua.asm
+++ udemy/continua.asm
@@ -49,4 +49,5 @@ saida:
     ;instruções obrigatórias para encerrar qualquer programa nasm
     mov     eax,0x1         ;indica o final de operação, corresponde a System.exit
     mov     ebx,0x0         ;informa o estado final do programa - 0 sem erro
-    int     0x80            ;implemente chamadas de sistema(syscall), uma forma de os programas do espaço do usuário se comunicarem com o kernel.
+    syscall                 ;implementa chamadas de sistema(syscall),
+                            ;uma forma de os programas do espaço do usuário se comunicarem com o kernel.
