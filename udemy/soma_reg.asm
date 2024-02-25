112cf8cd5493a873bed67a8ce98c4b0750067089
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-02-24 23:06:49 -0300

    update soma_reg.asm

--- udemy/soma_reg.asm
+++ udemy/soma_reg.asm
@@ -1,12 +1,13 @@
-
 section. data
-    num1 dd	0xFF
-    num2 dd	0xAA
-    ans dd	0x0
-    tam  equ $ - ans
-        
+    num1 dd	5
+    num2 dd	3
+      
     nline db 0xA,0     ;(db - pseudoinstrução - define uma cadeia de bytes)
     tam1 equ $ - nline
+
+section .bss
+    result resd 1
+    tam2 equ $ - result
 section .text
     global _start
 _start :
@@ -18,8 +19,8 @@ _start :
 
     mov eax, 0x4
     mov ebx, 0x1
-    mov ecx, ans
-    mov edx, tam           ;diz qual o tamanho do dado no endereço 'result'
+    mov ecx, result
+    mov edx, tam2           ;diz qual o tamanho do dado no endereço 'result'
     int 0x80                ;passa o comando para o SO
 
 ;operação de line feed 
