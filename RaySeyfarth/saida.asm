16ca9fe6e588e3ebb2b542de02a82d5525370bd8
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-02-24 22:41:40 -0300

    update soma_reg.asm

--- RaySeyfarth/saida.asm
+++ RaySeyfarth/saida.asm
@@ -9,7 +9,7 @@ segment .text
 
 global _start
 
-_start :
+_start:
         mov eax,1 ;1 é o número de chamada do sistema de saída
         mov ebx,3 ;o valor do status a ser retornado - se mudar aqui muda em "echo $?"
         int 0x80  ;executar uma chamada de sistema
--- udemy/soma_reg.asm
+++ udemy/soma_reg.asm
@@ -1,12 +1,16 @@
 section. data
-    num1	dword	11111111h
-    num2	dword	10101010h
+        num1	dword	11111111h
+        num2	dword	10101010h
 
 segment .text
 
 global _start
-
 _start :
+        mov	eax,num1
+		add eax,num2
+		mov	ans,eax    
+
+
         mov eax,0x1
         mov ebx,0x0 
         int 0x80  ;executar uma chamada de sistema
