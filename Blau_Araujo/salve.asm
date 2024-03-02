b85d02584540168456f2d7c24e5a1a448f97bf47
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-03-01 21:03:59 -0300

    update salve.asm(blau)

--- Blau_Araujo/salve.asm
+++ Blau_Araujo/salve.asm
@@ -3,7 +3,7 @@ section .data
     msg1 db "Não entre em pânico!",10           
     len1 equ $ - msg1
 
-    msg2 db "e sempre carregue uma toalha", 10  
+    msg2 db "e sempre carregue uma toalha.", 10,0  
     len2 equ $ - msg2
 
 section .text
